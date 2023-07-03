42551270b48d5a3cd81d28295413df205d4aafd9
kplnhsyn <kplnhsyn.49@gmail.com>
2023-07-03 07:23:40 +0200

    props_bootstrap_example

---
+++ rc_03_props_bootstrap_example/src/data.js
@@ -0,0 +1,129 @@
+export const contacts = [
+    {
+      id: 1,
+      name: "Hüseyin",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_cowboy_hat.png",
+      phone: "+1 123 123 1234",
+      email: "huseyin@react.com"
+    },
+    {
+      id: 2,
+      name: "Murat",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_wavy_hair_jacket_turtleneck.png",
+      phone: "+1 243 265 6524",
+      email: "murat@react.com"
+    },
+    {
+      id: 3,
+      name: "Gümüş",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_suit_blazer_tie_monocle.png",
+      phone: "+1 876 453 3344",
+      email: "gumus@react.com"
+    },
+    {
+      id: 4,
+      name: "Sinan",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_fashion_style_sunglasses_glasses_wavy_hair.png",
+      phone: "+1 243 265 6524",
+      email: "sinan@react.com"
+    },
+    {
+      id: 5,
+      name: "Soner Y.",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_wavy_hair_shirt_moustache_mustache_sunglasses_glasses.png",
+      phone: "+1 243 265 6524",
+      email: "soner_y@react.com"
+    },
+    {
+      id: 6,
+      name: "Bülent",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_angel_hoodie.png",
+      phone: "+1 243 265 6524",
+      email: "bulent@react.com"
+    },
+    {
+      id: 7,
+      name: "Tuba",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/female-avatars-4/128/avatars_accounts___female_woman_people_person_buns_earrings_shirt.png",
+      phone: "+1 243 265 6524",
+      email: "tuba@react.com"
+    },
+    {
+      id: 8,
+      name: "Kevser",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/female-avatars-4/128/avatars_accounts___female_woman_person_people_hoodie_earrings_medium_hair.png",
+      phone: "+1 243 265 6524",
+      email: "kevser@react.com"
+    },
+    {
+      id: 9,
+      name: "Mesut",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_angel_hoodie.png",
+      phone: "+1 243 265 6524",
+      email: "mesut@react.com"
+    },
+    {
+      id: 10,
+      name: "Burhan",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_hat_cap_baseball_cap_necklace_shirtless.png",
+      phone: "+1 243 265 6524",
+      email: "burhan@react.com"
+    },
+    {
+      id: 11,
+      name: "Norveçli",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_football_sport_sports_helmet.png",
+      phone: "+1 243 265 6524",
+      email: "norvec@react.com"
+    },
+    {
+      id: 12,
+      name: "Muhammed S.",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_turban_turbin_culture.png",
+        email: "muhammed_s@react.com",
+      phone: "+1 243 265 6524"
+    },
+    {
+      id: 13,
+      name: "Muhammed",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_beard_round_glasses_glasses_turtleneck_jacket.png",
+      phone: "+1 243 265 6524",
+      email: "muhammed@react.com"
+    },
+    {
+      id: 14,
+      name: "Seçkin",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_arabian_arab_sweater.png",
+      phone: "+1 243 265 6524",
+      email: "seckin@react.com"
+    },
+    {
+      id: 15,
+      name: "Abdullah",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_dark_african_chef_occupation.png",
+      phone: "+1 243 265 6524",
+      email: "abdullah@react.com"
+    },
+    {
+      id: 16,
+      name: "Eyüp",
+      imgURL:
+        "https://cdn2.iconfinder.com/data/icons/male-avatars/128/avatars_accounts___man_male_people_person_glasses_moustache_mustache_hairstyle_cardigan_sweater.png",
+      phone: "+1 243 265 6524",
+      email: "eyup@react.com"
+    }]
