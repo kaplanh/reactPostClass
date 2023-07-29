3cf1c4e123c85a21061e3666ad7709a86487333e
kplnhsyn <kplnhsyn.49@gmail.com>
2023-07-29 02:14:14 +0200

    LoginProvider

--- rc13_context-api/context-example1/context-example1-starter/src/pages/Login.jsx
+++ rc13_context-api/context-example1/context-example1-starter/src/pages/Login.jsx
@@ -1,48 +1,56 @@
-import { useState } from "react";
+import { useContext} from "react";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { LoginContext } from "../context/LoginContext";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [user, setUser] = useState({ email: "", password: "" });
+  const { user, setUser } = useContext(LoginContext)
+  const navigate =useNavigate()
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      navigate(-1)
+    };
 
-  return (
-    <Container>
-      <h1 className="text-center mt-4">LOGIN PAGE</h1>
-      <Form onSubmit={(e) => handleSubmit(e)}>
-        <Form.Group className="mb-3" controlId="username">
-          <Form.Label>Email</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter your email"
-            name="email"
-            value={user?.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
-          />
-        </Form.Group>
+    return (
+        <Container>
+            <h1 className="text-center mt-4">LOGIN PAGE</h1>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+                <Form.Group className="mb-3" controlId="username">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter your email"
+                        name="email"
+                        value={user?.email}
+                        onChange={(e) =>
+                            setUser({ ...user, email: e.target.value })
+                        }
+                    />
+                </Form.Group>
 
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter your password"
-            name="password"
-            value={user?.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-          />
-        </Form.Group>
-        <Container className="text-center">
-          <Button variant="danger" type="submit">
-            Submit
-          </Button>
+                <Form.Group className="mb-3" controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        placeholder="Enter your password"
+                        name="password"
+                        value={user?.password}
+                        onChange={(e) =>
+                            setUser({ ...user, password: e.target.value })
+                        }
+                    />
+                </Form.Group>
+                <Container className="text-center">
+                    <Button variant="danger" type="submit">
+                        Submit
+                    </Button>
+                </Container>
+            </Form>
         </Container>
-      </Form>
-    </Container>
-  );
+    );
 };
 
 export default Login;
--- rc13_context-api/context-example1/context-example1-starter/src/pages/PrivateRouter.jsx
+++ rc13_context-api/context-example1/context-example1-starter/src/pages/PrivateRouter.jsx
@@ -1,8 +1,11 @@
+import { useContext } from "react";
 import { Outlet, Navigate } from "react-router-dom";
+import { LoginContext } from "../context/LoginContext";
 
 const PrivateRouter = () => {
-  const user = true;
-  return user ? <Outlet /> : <Navigate to="/login" />;
+    // const user = true;
+    const { user } = useContext(LoginContext);
+    return user.email&&user.password ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default PrivateRouter;
